cbb8dcd04f0098cd8a1f500dd1f5fbcbcb94bba2
nrymarz <nrymarz@gmail.com>
2021-01-07 00:04:06 -0600

    commit tree 610521d54e3459fb2dcf7d32cd25ee9c835eae93

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ActiveRecord::Base
-  
+    belongs_to :user
+    has_many :post_tags
+    has_many :tags, through: :post_tags
+    accepts_nested_attributes_for :tags
+
+    validates :name, presence: true
+    validates :content, presence: true
+
+    def tag_attributes=(tag_attributes)
+        if !tag_attributes[:name].empty?
+            tag = Tag.find_or_create_by(name: tag_attributes[:name])
+            self.tags << tag 
+        end
+    end
 end
